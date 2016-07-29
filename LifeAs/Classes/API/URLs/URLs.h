e9d9e20202c5a902acfe1c63d9d799d9e46e8d43
lyleKP <v2top1@163.com>
2016-07-29 17:19:03 +0800

    确认接口地址

---
+++ LifeAs/Classes/API/URLs/URLs.h
@@ -0,0 +1,13 @@
+//
+//  URLs.h
+//  LifeAs
+//
+//  Created by lyleKP on 16/7/29.
+//  Copyright © 2016年 lyleKP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface URLs : NSObject
+
+@end
---
+++ LifeAs/Classes/API/URLs/URLs.m
@@ -0,0 +1,13 @@
+//
+//  URLs.m
+//  LifeAs
+//
+//  Created by lyleKP on 16/7/29.
+//  Copyright © 2016年 lyleKP. All rights reserved.
+//
+
+#import "URLs.h"
+
+@implementation URLs
+
+@end
