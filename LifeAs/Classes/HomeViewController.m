d728ba319e4d623e58e048897a024d35dbb26d00
lyleKP <v2top1@163.com>
2016-07-29 17:44:57 +0800

    测试代码，待删除

--- LifeAs/Classes/HomeViewController.m
+++ LifeAs/Classes/HomeViewController.m
@@ -18,6 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.title = @"Home";
+//    [KPNetworkManager sharedManager]
+    NSLog(@"%@",kCDKAPIScheme);
+    
 }
 
 
